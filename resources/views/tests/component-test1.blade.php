d302432b5c0df8980d75f619d96805e32c6e7d6b
OkunoEiji <e.okuno@nextmake.co.jp>
2024-04-10 02:57:23 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
 コンポ―ネントテスト1
     <x-tests.card title="タイトル" content="本文" :message="$message" />
     <x-tests.card title="タイトル" />
+    <x-tests.card title="CSSを変更したい" class="bg-red-200" />
 </x-tests.app>
