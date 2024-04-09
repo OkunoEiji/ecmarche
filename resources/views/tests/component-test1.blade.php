31bde0ae6bffdab1cdfa00c05ff051ac738344e7
OkunoEiji <e.okuno@nextmake.co.jp>
2024-04-10 02:29:13 +0900

    名前付きスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+    <x-slot name="header">
+        ヘッダー1
+    </x-slot>
 コンポ―ネントテスト1
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+    <x-slot name="header">
+        ヘッダー2
+    </x-slot>
 コンポ―ネントテスト2
 </x-tests.app>
