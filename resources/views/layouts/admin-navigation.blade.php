988de43f62d1d79d5864d3c3848cc5f692dcb3f6
OkunoEiji <e.okuno@nextmake.co.jp>
2024-04-11 14:28:12 +0900

    アプリ名、ロゴ

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -88,7 +88,7 @@
                 <form method="POST" action="{{ route('admin.logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('admin.logout')"
+                    <x-responsive-nav-link :href="route('admin.ogout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
