80104e8ce48c3d594dc0d860ca94ed9639a59561
OkunoEiji <e.okuno@nextmake.co.jp>
2024-04-19 04:59:25 +0900

    stripe controller

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Cart;
 use App\Models\User;
+use App\Models\Stock;
 
 class CartController extends Controller
 {
@@ -53,5 +54,59 @@ class CartController extends Controller
         ->delete();
 
         return redirect()->route('user.cart.index');
+    }
+
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        
+        $lineItems = [];
+        foreach($products as $product){
+            $quantity = '';
+            $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+
+            if($product->pivot->quantity > $quantity){
+                return redirect()->route('user.cart.index');
+            } else {
+                $price_data = ([
+                    'unit_amount' => $product->price,
+                    'currency' => 'jpy',
+                    'product_data' => $product_data = ([
+                        'name' => $product->name,
+                        'description' => $product->information,
+                    ]),
+                ]);
+    
+                $lineItem = [
+                    'price_data' => $price_data,
+                    'quantity' => $product->pivot->quantity,
+                ];
+                array_push($lineItems, $lineItem);    
+            }
+        }
+
+        foreach($products as $product){
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1
+            ]);
+        }
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'], // 支払方法を指定
+            'line_items' => [$lineItems], // $lineItemsを定義
+            'mode' => 'payment', // 支払モードの選択
+            'success_url' => route('user.items.index'), // 成功ページroute('user.cart.success'), 
+            'cancel_url' => route('user.cart.index'), // キャンセルページroute('user.cart.cancel'), 
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout', 
+            compact('session', 'publicKey'));
     }
 }
